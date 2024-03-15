3477c176ab1fc4ffb84e610e0b96d6315c39a7f7
iannjau <lantanacreations@gmail.com>
2024-03-15 14:31:41 +0300

    add index .js

---
+++ index.js
@@ -0,0 +1,9 @@
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`)
+}
+let name ="Ian"
+let language ="Javascript"
+console.log ("Hi ,my name is "+name+" and I am learning to program in "+language+" ")
+
+require ( './root.js' );
+describe('introduction(name)', function() it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() ) )
